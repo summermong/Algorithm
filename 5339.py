9a9d159eada3d8c1d0f6ff6d76f782f3616be651
summermong <124887974+summermong@users.noreply.github.com>
2023-02-22 20:24:56 +0900

    백준 알고리즘 5339번

---
+++ 5339.py
@@ -0,0 +1,21 @@
+a = '     /~\\'
+b = '    ( oo|'
+c = '    _\=/_'
+d = '   /  _  \\'
+e = '  //|/.\|\\\\'
+f = ' ||  \ /  ||'
+g = '============'
+h = '|          |'
+i = '|          |'
+j = '|          |'
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
