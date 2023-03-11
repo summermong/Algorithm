a2b1464d0f1dae5b0a5f5a2f08f30fde33af7cd6
summermong <2514o_o@naver.com>
2023-03-11 23:48:47 +0900

    0311 알고리즘 풀이

---
+++ 13597.py
@@ -0,0 +1,8 @@
+card1, card2 = map(int, input().split())
+
+if card1 > card2 :
+    print(card1, end='')
+elif card1 < card2 :
+    print(card2, end='')
+elif card1 == card2 :
+    print(card1, end='')
