c91c634de32bab426fcee9b0f13e55091582b7b2
summermong <124887974+summermong@users.noreply.github.com>
2023-03-22 01:25:29 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,7 @@
 function solution(n) {
     let num = String(n);
-    let mapfn = (i) => Number(i);
-    let newArr = Array.from(num, mapfn);
     let sum = 0;
-    for (var i = 0; i < newArr.length; i++) {
-        sum += newArr[i]
-    }
-    return sum
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    } return sum
 }
