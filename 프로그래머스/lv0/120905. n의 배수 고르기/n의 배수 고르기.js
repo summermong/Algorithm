cc9b5264b734434cef3154185ab990a276b52441
summermong <124887974+summermong@users.noreply.github.com>
2023-03-24 14:07:58 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,8 +1,3 @@
 function solution(n, numlist) {
-    let answer = [];
-    for (let i = 0; i < numlist.length; i++) {
-        if (numlist[i] % n === 0) {
-            answer.push(numlist[i])
-        }
-    } return answer
+    return numlist.filter((item) => item % n === 0)
 }
