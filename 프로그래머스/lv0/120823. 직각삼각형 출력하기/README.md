463133186f4e741d6a1af9ebff02ae3604a3f8ba
summermong <124887974+summermong@users.noreply.github.com>
2023-03-25 00:44:10 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 64.91 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/README.md
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 직각삼각형 출력하기 - 120823 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120823) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120823#) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 56.76 ms
+메모리: 32.3 MB, 시간: 64.91 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -1,6 +1,14 @@
-function solution(n) {
-    let answer = [];
-    for (let i = 1; i < n; i++) {
-        answer.push((n-(n-i)) * '*')
-    } return answer.join('')
-}
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    let inputNum = Number(input[0]);
+    for (let i = 1; i <= inputNum; i++) {
+        console.log('*'.repeat(i))
+    }
+});
