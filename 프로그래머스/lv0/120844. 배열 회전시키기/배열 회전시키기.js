501219bf9cfd675d8c32038fe6976dd4ff62598d
summermong <124887974+summermong@users.noreply.github.com>
2023-03-25 15:47:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,13 +1,13 @@
 function solution(numbers, direction) {
-    if (direction === 'right') {
-        let tmp = numbers[numbers.length-1];
-        numbers.splice(-1, 1);
-        numbers.unshift(tmp);
-        return numbers
+    var answer = [];
+
+    if ("right" == direction) {
+        numbers.unshift(numbers.pop());
     } else {
-        let tmp = numbers[0];
-        numbers.splice(0, 1);
-        numbers.push(tmp);
-        return numbers
+        numbers.push(numbers.shift());
     }
+
+    answer = numbers;
+
+    return answer;
 }
