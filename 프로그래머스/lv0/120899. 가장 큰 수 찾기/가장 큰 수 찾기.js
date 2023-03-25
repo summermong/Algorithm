1c2b27121106def3f21f340279661ec662a5326b
summermong <124887974+summermong@users.noreply.github.com>
2023-03-25 15:53:28 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,8 +1,4 @@
 function solution(array) {
-    let arr = [];
     let a = Math.max(...array);
-    let b = array.indexOf(a);
-    arr.push(a)
-    arr.push(b)
-    return arr
+    return [a, array.indexOf(a)]
 }
