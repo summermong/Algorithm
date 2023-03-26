f699000b48a24e557ab757e74417c0443a6ebd9a
summermong <124887974+summermong@users.noreply.github.com>
2023-03-26 22:24:10 +0900

    [level 0] Title: 369게임, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,9 +1,4 @@
 function solution(order) {
-    let str = order.toString();
-    let clap = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== '0' && str[i] % 3 === 0) {
-            clap += 1
-        }
-    } return clap
+    let ans = [...order.toString().matchAll(/[3|6|9]/g)].length
+    return ans
 }
