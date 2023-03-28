40d77e7832cb43f11965209e51aeb8f270cd87bd
summermong <124887974+summermong@users.noreply.github.com>
2023-03-29 00:07:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,26 +1,18 @@
-function isAlpha(c) {
-  return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z");
-}
-
-function isDigit(c) {
-  return c >= "0" && c <= "9";
-}
-
 function solution(my_string) {
-  let answer = 0;
-
-  let num = "";
-
-  for (let i = 0; i < my_string.length; i++) {
-    if (isDigit(my_string[i])) {
-      num += my_string[i];
+    let a = '';
+    let b = [];
+    let c = 0;
+    for (let i = 0; i < my_string.length; i++) {
+        if (isNaN(my_string[i])) {
+            my_string = my_string.replace(my_string[i], ' ')
+        }
+    } a = my_string.split(' ')
+    for (let i of a) {
+        if (i !== '') {
+            b.push(i)
+        }
     }
-    if (isAlpha(my_string[i]) || i === my_string.length - 1) {
-      if (num !== "") {
-        answer += parseInt(num);
-        num = "";
-      }
-    }
-  }
-  return answer;
+    for (let i = 0; i < b.length; i++) {
+        c += Number(b[i])
+    } return c
 }
