93e8042121ba09eeca8ba6f13ee25fdb8c8d0188
summermong <124887974+summermong@users.noreply.github.com>
2023-03-29 00:16:29 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,18 +1,9 @@
 function solution(my_string) {
-    let a = '';
-    let b = [];
-    let c = 0;
+    let sum = 0;
+    my_string = my_string.replace(/[A-z]/g, " ").split(" ")
     for (let i = 0; i < my_string.length; i++) {
-        if (isNaN(my_string[i])) {
-            my_string = my_string.replace(my_string[i], ' ')
+        if (my_string[i] !== "") {
+            sum += Number(my_string[i])
         }
-    } a = my_string.split(' ')
-    for (let i of a) {
-        if (i !== '') {
-            b.push(i)
-        }
-    }
-    for (let i = 0; i < b.length; i++) {
-        c += Number(b[i])
-    } return c
+    } return sum
 }
