c84affac7b3f498d4adfde4d9bc7c0a8a913cbe7
summermong <124887974+summermong@users.noreply.github.com>
2023-03-31 18:08:30 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/README.md
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 진료 순서 정하기 - 120835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835#) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 3.13 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,23 +1,4 @@
 function solution(emergency) {
-    let sortedEmergency = emergency.slice().sort((a, b) => a - b);
-    
-    const answer = [];
-    for (let i = 0; i < emergency.length; i++) {
-        answer.push(-1)
-    }
-    
-    let rank = 1;
-
-    while (sortedEmergency.length !== 0) {
-        const maxValue = sortedEmergency.pop();
-        const maxValueIndex = emergency.indexOf(maxValue);
-        answer[maxValueIndex] = rank;
-        rank += 1;
-
-    }
-    console.log(" sorted : ", sortedEmergency);
-    console.log("emegency : ", emergency);
-    console.log("answer : " , answer)
-    
-    return answer;
+    let sortedEmergency = [...emergency].sort((a, b) => b-a)
+    return emergency.map((item) => sortedEmergency.indexOf(item)+1)
 }
