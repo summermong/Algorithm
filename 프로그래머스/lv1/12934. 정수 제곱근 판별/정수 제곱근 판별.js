66449b94f3a15f802a714d0e09fa1f4dfc191cb6
summermong <124887974+summermong@users.noreply.github.com>
2023-04-04 14:30:11 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,6 +1,6 @@
 function solution(n) {
     let nSQRT = Math.ceil(Math.sqrt(n))
-    if (nSQRT >= 0 && nSQRT * nSQRT === n) {
+    if (nSQRT * nSQRT === n) {
         return Math.pow(nSQRT+1, 2)
     } else {
         return -1
