556a91bbbd358682c0777bca4743a60284342f93
summermong <124887974+summermong@users.noreply.github.com>
2023-04-06 11:59:41 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+  let answer = "";
+  let leng = phone_number.length - 4;
+  let lastNumber = [...phone_number].splice(leng, 4);
+
+  return "*".repeat(leng) + lastNumber.join("");
+}
