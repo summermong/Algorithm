967295540f6e1f84f156b714b508409d34fc879a
summermong <124887974+summermong@users.noreply.github.com>
2023-04-14 18:37:35 +0900

    [level 1] Title: 최소직사각형, Time: 3.39 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,15 +1,12 @@
 function solution(sizes) {
-  let max_width = 0;
-  let max_height = 0;
-  
-  // 가장 큰 가로 길이와 세로 길이를 구합니다.
+  let answerMax = [];
+  let answerMin = [];
+
   for (let i = 0; i < sizes.length; i++) {
-    let size = sizes[i];
-    max_width = Math.max(max_width, Math.max(...size));
-    max_height = Math.max(max_height, Math.min(...size));
+    let maxWidth = Math.max(sizes[i][0], sizes[i][1]);
+    answerMax.push(maxWidth);
+    let minWidth = Math.min(sizes[i][0], sizes[i][1]);
+    answerMin.push(minWidth);
   }
-  
-  // 가로 길이가 더 긴 경우와 세로 길이가 더 긴 경우를 모두 고려하여
-  // 가장 작은 지갑 크기를 계산합니다.
-  return max_width * max_height;
+  return Math.max(...answerMax) * Math.max(...answerMin);
 }
