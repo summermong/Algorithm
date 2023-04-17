422b052a2ebc0739f29c0e0d627b3852ede2043c
summermong <124887974+summermong@users.noreply.github.com>
2023-04-17 23:42:36 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,17 @@
+function solution(s, n) {
+  return s.sort((a, b) => {
+    if (a[n] > b[n]) {
+      return 1;
+    } else if (a[n] < b[n]) {
+      return -1;
+    } else {
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+}
