0897dda732defbc1ea9c24d0b6863337aa697a97
summermong <124887974+summermong@users.noreply.github.com>
2023-04-18 17:08:28 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+  let answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let arr = array.slice(commands[i][0] - 1, commands[i][1]);
+    arr.sort((a, b) => a - b);
+    answer.push(Number(arr.splice(commands[i][2] - 1, 1)));
+  }
+  return answer;
+}
