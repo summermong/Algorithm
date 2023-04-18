c444782e2fdfced0a591544a8415e8da216d2fad
summermong <124887974+summermong@users.noreply.github.com>
2023-04-19 00:19:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.75 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,19 +2,20 @@ function solution(numbers) {
   let answer = [];
   let answer2 = [];
 
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      if (i !== j) {
-        let ele = numbers[i] + numbers[j];
-        answer.push(ele);
-      }
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let ele = numbers[i] + numbers[j];
+      answer.push(ele);
     }
   }
+  
   answer.sort((a, b) => a - b);
+  
   for (let i = 0; i < answer.length; i++) {
     if (answer[i] !== answer[i + 1]) {
       answer2.push(answer[i]);
     }
   }
+  
   return answer2;
 }
