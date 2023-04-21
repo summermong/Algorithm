279f59c44a43f14f2844e52f74cf887e70d04f3e
summermong <124887974+summermong@users.noreply.github.com>
2023-04-21 16:45:51 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,7 @@
 function solution(str1, str2) {
   let answer = "";
-  let i = 0;
-  while (i < str1.length) {
+  for (let i = 0; i < str1.length; i++) {
     answer += str1[i] + str2[i];
-    i++;
   }
   return answer;
 }
