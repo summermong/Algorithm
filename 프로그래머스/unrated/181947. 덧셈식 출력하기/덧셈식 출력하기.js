ddcfe1c7879a88e960be44bd8dd7aa7705682f8e
summermong <124887974+summermong@users.noreply.github.com>
2023-04-21 15:53:37 +0900

    [unrated] Title: 덧셈식 출력하기, Time: 62.18 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -1,13 +1,18 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    console.log(input[0] + ' + ' + input[1] + ' = ' + (Number(input[0]) + Number(input[1])));
+rl.on("line", function (line) {
+  input = line.split(" ");
+  rl.close();
+}).on("close", function () {
+  console.log(
+    `${Number(input[0])} + ${Number(input[1])} = ${
+      Number(input[0]) + Number(input[1])
+    } `
+  );
 });
