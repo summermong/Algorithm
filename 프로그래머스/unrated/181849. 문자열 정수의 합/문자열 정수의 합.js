c4cc253145791b5092d629ba472b1f5921111203
summermong <124887974+summermong@users.noreply.github.com>
2023-04-23 01:57:50 +0900

    [unrated] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,8 +1,5 @@
 function solution(num_str) {
     let sum = 0;
-    let splitNumstr = num_str.split('')
-    for (let i = 0; i < splitNumstr.length; i++) {
-        sum += Number(splitNumstr[i])
-    }
+    num_str.split('').map(i => sum += Number(i))
     return sum
 }
