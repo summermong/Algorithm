6afd70507169f3880fda74e03071b902a1ddf276
summermong <124887974+summermong@users.noreply.github.com>
2023-04-23 01:33:34 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 2.57 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,12 +1,3 @@
 function solution(arr, k) {
-    let answer = []
-    if (k % 2 === 0) {
-        for (let i = 0; i < arr.length; i++) {
-            answer.push(arr[i]+k)
-        }
-    } else {
-        for (let i = 0; i < arr.length; i++) {
-            answer.push(arr[i]*k)
-        }
-    } return answer
+    return arr.map(i => k % 2 === 0 ? i+k : i*k)
 }
