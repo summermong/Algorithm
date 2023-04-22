c3ccc2efe44d59f06201a4abb971e5f4edd50f7a
summermong <124887974+summermong@users.noreply.github.com>
2023-04-23 02:08:10 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -2,7 +2,6 @@ function solution(my_string, index_list) {
     let answer = ''
     
     for (let i = 0; i < index_list.length; i++) {
-        let idx = index_list[i]
-        answer += my_string[idx]
+        answer += my_string[index_list[i]]
     } return answer
 }
