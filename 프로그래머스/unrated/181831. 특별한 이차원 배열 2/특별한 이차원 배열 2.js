2c8e5b0bc630a2a6eb11cbe4da49fe54d563f954
summermong <124887974+summermong@users.noreply.github.com>
2023-04-25 16:35:58 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,15 +1,12 @@
 function solution(arr) {
-    let answer = 1;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i][j] !== arr[j][i]) {
-                answer = 0;
-                break;
+    var answer = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j< arr.length; j++) {
+            if(arr[i][j] !== arr[j][i]) {
+                return 0;
             }
         }
-        if (answer === 0) {
-            break;
-        }
     }
-    return answer;
+    return 1;
 }
