1565bc96a1962ab17a2fa9440baaedb7c4a79b6b
summermong <124887974+summermong@users.noreply.github.com>
2023-04-25 15:40:53 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,9 +1,3 @@
 function solution(my_string) {
-    let answer = ''
-    let space = my_string.split(' ')
-    for (let i = 0; i < space.length; i++) {
-        if (space[i] !== '') {
-            answer += space[i] + ' '
-        }
-    } return answer.trimEnd().split(' ')
+    return my_string.split(' ').filter((v) => v !== '');
 }
