94bc212454115e572a64091f82968f25eb436e41
summermong <124887974+summermong@users.noreply.github.com>
2023-04-25 13:18:05 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.87 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,5 +1,7 @@
 function solution(arr) {
-    return arr.map((v) =>
-                  v >= 50 && v % 2 === 0 ? parseInt(v / 2) :
-                  v < 50 && v % 2 !== 0 ? parseInt(v * 2) : v)
+    return arr.map(num => {
+        if (num >= 50 && num % 2 === 0) return num / 2;
+        if (num <= 50 && num % 2 !== 0) return num * 2
+        return num
+    })
 }
