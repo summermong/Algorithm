be967389015223a8145824bc03b2a429261185bc
summermong <124887974+summermong@users.noreply.github.com>
2023-04-25 15:19:08 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,16 +1,15 @@
 function solution(n) {
-    let sum = 0;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
     if (n % 2 !== 0) {
-        for (let i = 1; i <= n; i++) {
-            if (i % 2 !== 0) {
-                sum += i
-            }
-        }
+      if (i % 2 !== 0) {
+        sum += i;
+      }
     } else {
-        for (let i = 1; i <= n; i++) {
-            if (i % 2 === 0) {
-                sum += Math.pow(i, 2)
-            }
-        }
-    } return sum
+      if (i % 2 === 0) {
+        sum += Math.pow(i, 2);
+      }
+    }
+  }
+  return sum;
 }
