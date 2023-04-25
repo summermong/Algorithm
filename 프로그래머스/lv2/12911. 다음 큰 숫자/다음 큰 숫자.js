aa936f43d3f2375a286cd676a1a6f1370693cd3b
summermong <124887974+summermong@users.noreply.github.com>
2023-04-26 02:09:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let count = 0;
+  let n2 = n.toString(2);
+  let n2length = [...n2].filter((v) => v === "1").length;
+
+  for (let i = n + 1; i <= 1000000; i++) {
+    let i2 = i.toString(2);
+    let i2length = [...i2].filter((v) => v === "1").length;
+
+    if (n2length === i2length) {
+      return i;
+    }
+  }
+}
