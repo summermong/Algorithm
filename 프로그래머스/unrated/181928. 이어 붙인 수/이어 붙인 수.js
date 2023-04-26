dee6e072a7d3f70ac92ed0f9d192f2ccafa0451c
summermong <124887974+summermong@users.noreply.github.com>
2023-04-26 18:50:04 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let odd = ''
+    let even = ''
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 !== 0) {
+            odd += String(num_list[i])
+        } else {
+            even += String(num_list[i])
+        }
+    } return Number(odd) + Number(even)
+}
