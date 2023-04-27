86f99f82305f8742851e5b51597b2a6e1172dcfb
summermong <124887974+summermong@users.noreply.github.com>
2023-04-28 00:45:54 +0900

    [unrated] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,3 @@
 function solution(todo_list, finished) {
-    let answer = []
-    
-    for (let i = 0; i < todo_list.length; i++) {
-        if (finished[i] === false) {
-            answer.push(todo_list[i])
-        }
-    } return answer
+    return todo_list.filter((_, i) => !finished[i])
 }
