9e88961a3d0e7feb50e8498757be0bfec2cde16e
summermong <124887974+summermong@users.noreply.github.com>
2023-04-28 01:30:39 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = []
+    answer.push(n)
+    
+    while (n > 1) {
+        if (n % 2 === 0) {
+            n = n / 2
+            answer.push(n)
+        } else {
+            n = n*3 +1
+            answer.push(n)
+        }
+    } return answer
+}
