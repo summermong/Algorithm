462ec5eff865a5ad493006634521c7325cd895ce
summermong <124887974+summermong@users.noreply.github.com>
2023-04-27 21:33:25 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,11 +1,8 @@
 function solution(num_list) {
     let oddSum = 0;
     let evenSum = 0;
-    for (let i = 0; i < num_list.length; i++) {
-        if (i === 0 || i % 2 === 0) {
-            oddSum += num_list[i]
-        } else {
-            evenSum += num_list[i]
-        }
-    } return oddSum >= evenSum ? oddSum : evenSum
+    num_list.map((v, i) => {
+        i % 2 === 0 ? oddSum += v : evenSum += v
+    })
+    return oddSum > evenSum ? oddSum : evenSum
 }
