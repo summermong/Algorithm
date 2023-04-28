1285dcd133b0e4792677a12f315b50f619d06d23
summermong <124887974+summermong@users.noreply.github.com>
2023-04-28 23:40:20 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,19 +1,5 @@
 function solution(rank, attendance) {
-    let students = []
-    let answer = []
-    
-    for (let i = 0; i < attendance.length; i++) {
-        if (attendance[i] === true) {
-            students.push(rank[i])
-        }
-    } 
-    students.sort((a, b) => a-b)
-    let result = students.slice(0, 3)
-    
-    for (let i = 0; i < result.length; i++) {
-        let ranking = rank.indexOf(result[i])
-        answer.push(ranking)
-    } 
-    
-    return 10000 * answer[0] + 100 * answer[1] + answer[2]
+  const students = rank.filter((_, i) => attendance[i]).sort((a, b) => a - b);
+  const answer = students.slice(0, 3).map(s => rank.indexOf(s));
+  return 10000 * answer[0] + 100 * answer[1] + answer[2];
 }
