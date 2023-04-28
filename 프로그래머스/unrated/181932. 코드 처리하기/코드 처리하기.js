f0e350c28cd2c2515555cd21d6c85fce9dd81069
summermong <124887974+summermong@users.noreply.github.com>
2023-04-28 23:22:05 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,19 @@
+function solution(code) {
+  let ret = "";
+  let mode = 0;
+
+  for (let idx = 0; idx < code.length; idx++) {
+    if (code[idx] === "1") {
+      mode = mode === 0 ? 1 : 0;
+      continue;
+    }
+
+    if (mode === 0 && idx % 2 === 0) {
+      ret += code[idx];
+    } else if (mode === 1 && idx % 2 === 1) {
+      ret += code[idx];
+    }
+  }
+
+  return ret || "EMPTY";
+}
