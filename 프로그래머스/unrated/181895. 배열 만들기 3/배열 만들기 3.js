1e26c4b35194a516b4f670315d211a180d91234f
summermong <124887974+summermong@users.noreply.github.com>
2023-04-28 14:53:05 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.12 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,5 @@
+function solution(arr, intervals) {
+    let first = arr.slice(intervals[0][0], intervals[0][1]+1)
+    let second = arr.slice(intervals[1][0], intervals[1][1]+1)
+    return first.concat(second)
+}
