97693a59642618e5cb3a348e7ed0eef740ea1259
summermong <124887974+summermong@users.noreply.github.com>
2023-05-30 16:17:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 104.26 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -5,6 +5,7 @@ function solution(participant, completion) {
     
   let arr1 = participant.sort();
   let arr2 = completion.sort();
+  
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
