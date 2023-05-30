70abcc54b9dafa3655829467394127986448928b
summermong <124887974+summermong@users.noreply.github.com>
2023-05-30 23:31:11 +0900

    [level 1] Title: 모의고사, Time: 1.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,44 @@
+function solution(answers) {
+  let answer = [];
+  let person1 = 0;
+  let person2 = 0;
+  let person3 = 0;
+
+  let one = [1, 2, 3, 4, 5];
+  let two = [2, 1, 2, 3, 2, 4, 2, 5];
+  let three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === one[i % one.length]) {
+      person1 += 1;
+    }
+    if (answers[i] === two[i % two.length]) {
+      person2 += 1;
+    }
+    if (answers[i] === three[i % three.length]) {
+      person3 += 1;
+    }
+  }
+
+  if (person1 == person2 && person2 == person3) {
+    answer.push(1);
+    answer.push(2);
+    answer.push(3);
+  } else if (person1 > person2 && person1 > person3) {
+    answer.push(1);
+  } else if (person2 > person1 && person2 > person3) {
+    answer.push(2);
+  } else if (person3 > person1 && person3 > person2) {
+    answer.push(3);
+  } else if (person1 == person2 && person1 !== person3) {
+    answer.push(1);
+    answer.push(2);
+  } else if (person2 == person3 && person2 !== person1) {
+    answer.push(2);
+    answer.push(3);
+  } else if (person3 == person1 && person3 !== person2) {
+    answer.push(3);
+    answer.push(1);
+  }
+  return answer.sort((a, b) => a - b);
+}
