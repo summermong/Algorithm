ce9a1679f526d3d2ed7bbb6732d1ed16299078d3
summermong <124887974+summermong@users.noreply.github.com>
2023-06-01 23:07:56 +0900

    [level 1] Title: 2016년, Time: 0.28 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,20 +1,4 @@
 function solution(a, b) {
-  let month1stWeek = [
-    "FRI",
-    "MON",
-    "TUE",
-    "FRI",
-    "SUN",
-    "WED",
-    "FRI",
-    "MON",
-    "THU",
-    "SAT",
-    "TUE",
-    "THU",
-  ]; //각 월의 1일 요일
-  let weekend = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
-  let matchWeek = weekend.indexOf(month1stWeek[a - 1]) + ((b - 1) % 7);
-  return weekend[matchWeek % 7];
+    let day = new Date(2016, a-1, b)
+    return day.toString().slice(0, 3).toUpperCase()
 }
--- 프로그래머스/lv1/12901. 2016년/README.md
+++ 프로그래머스/lv1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.04 ms
+메모리: 34.5 MB, 시간: 0.28 ms
 
 ### 구분
 
