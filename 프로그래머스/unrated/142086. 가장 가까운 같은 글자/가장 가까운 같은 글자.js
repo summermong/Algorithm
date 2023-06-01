6854aae3586726c5f6b5e40500afb2a3a204962e
summermong <124887974+summermong@users.noreply.github.com>
2023-06-01 10:33:58 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 6.27 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,6 +1,22 @@
-const solution = (s) =>
-  [...s].map((char, i) => {
-    const count = s.slice(0, i).lastIndexOf(char);
-    console.log(count)
-    return count < 0 ? count : i - count;
-  });
+function solution(s) {
+    let arr = []
+    let answer = [] //정답 반환할 배열
+    
+    for (let i = 0; i < s.length; i++) {
+        let ele = arr.lastIndexOf(s.substr(i, 1))
+        
+        // i = 0, 1, 2, 3, 4, 5
+        // ele = -1, -1, -1, 1, 2, 3
+        
+        arr.push(s.substr(i, 1))
+        
+        // arr = ['b', 'a', 'n', 'a', 'n', 'a']
+        
+        if (ele === -1) {
+            answer.push(ele)
+        } else {
+            answer.push(answer.length-ele)
+        }
+        // answer = [-1, -1, -1, 3-1, 4-2, 5-3 ]
+    } return answer
+}
