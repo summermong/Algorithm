af22a02c9c7e872fe801962d74f7651ac8ddd486
summermong <124887974+summermong@users.noreply.github.com>
2023-06-06 23:59:41 +0900

    [unrated] Title: 둘만의 암호, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,7 +1,7 @@
 function solution(s, skip, index) {
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
-    skip.split('').forEach((v) => {
+    skip.split('').map((v) => {
         alphabet.splice(alphabet.indexOf(v), 1)
     })
     
