de77504e97be3bcbeb9ac02c0437f179c4ceeb87
summermong <124887974+summermong@users.noreply.github.com>
2023-06-14 03:17:59 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,20 +1,21 @@
 function solution(n, lost, reserve) {
-  let realLost = lost.filter((v) => !reserve.includes(v)).sort((a, b) => a - b);
-  let canBorrow = reserve.filter((v) => !lost.includes(v)).sort((a, b) => a - b);
+  let realLost = lost.filter((v) => !reserve.includes(v)).sort((a, b) => a-b);
+  let canBorrow = reserve.filter((v) => !lost.includes(v)).sort((a, b) => a-b);
 
-  let answer = n - realLost.length;
+
 
   for (let i = 0; i < canBorrow.length; i++) {
-    const borrower = canBorrow[i];
+    let borrower = canBorrow[i];
 
     if (realLost.includes(borrower - 1)) {
       realLost.splice(realLost.indexOf(borrower - 1), 1);
-      answer++;
+
     } else if (realLost.includes(borrower + 1)) {
       realLost.splice(realLost.indexOf(borrower + 1), 1);
-      answer++;
+
     }
   }
 
-  return answer;
+
+  return n-realLost.length;
 }
