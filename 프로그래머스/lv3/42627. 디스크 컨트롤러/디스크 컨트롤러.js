5cb4a3bdf9198e39601ef6f373c183d40b0f8406
summermong <124887974+summermong@users.noreply.github.com>
2023-06-19 16:33:55 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,51 +1,27 @@
-function solution(jobs) {
-  const len = jobs.length;
-  let answer = 0;
-  let currentTime = 0;
-  let totalWaitingTime = 0;
-
-  // 작업의 요청 시간과 작업 시간을 기준으로 정렬
+const solution = (jobs) => {
+  let answer = 0,
+    j = 0,
+    time = 0;
   jobs.sort((a, b) => {
-    if (a[0] === b[0]) {
-      return a[1] - b[1];
-    } else {
-      return a[0] - b[0];
-    }
+    return a[0] - b[0];
   });
 
-  while (jobs.length > 0) {
-    let index = -1;
-    let minTime = 1001; // 최대 작업 소요시간보다 큰 초기값
-
-    // 현재 시간 이전에 요청된 작업들 중에서 가장 작업 소요시간이 짧은 작업 선택
-    for (let i = 0; i < jobs.length; i++) {
-      if (jobs[i][0] <= currentTime && jobs[i][1] < minTime) {
-        minTime = jobs[i][1];
-        index = i;
-      }
-    }
-
-    // 선택된 작업이 없는 경우 현재 시간을 1 증가시킴
-    if (index === -1) {
-      currentTime++;
+  const priorityQueue = [];
+  while (j < jobs.length || priorityQueue.length !== 0) {
+    if (jobs.length > j && time >= jobs[j][0]) {
+      priorityQueue.push(jobs[j++]);
+      priorityQueue.sort((a, b) => {
+        return a[1] - b[1];
+      });
       continue;
     }
-
-    // 현재 시간보다 작업이 요청된 시간이 늦을 경우 대기 시간 누적
-    if (currentTime < jobs[index][0]) {
-      totalWaitingTime += jobs[index][0] - currentTime;
-      currentTime = jobs[index][0];
+    if (priorityQueue.length !== 0) {
+      time += priorityQueue[0][1];
+      answer += time - priorityQueue[0][0];
+      priorityQueue.shift();
+    } else {
+      time = jobs[j][0];
     }
-
-    // 해당 작업의 요청부터 종료까지 걸린 시간을 누적하여 answer에 추가
-    answer += currentTime - jobs[index][0] + jobs[index][1];
-
-    // 현재 시간을 작업 소요시간만큼 증가
-    currentTime += jobs[index][1];
-
-    // 선택한 작업 제거
-    jobs.splice(index, 1);
   }
-
-  return Math.floor(answer / len);
-}
+  return parseInt(answer / jobs.length);
+};
