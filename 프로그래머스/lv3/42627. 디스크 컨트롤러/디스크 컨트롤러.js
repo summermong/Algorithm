d1ceb626037fcac9f89ba177e1243b3e120ccc5a
summermong <124887974+summermong@users.noreply.github.com>
2023-06-20 12:41:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,49 +1,29 @@
 function solution(jobs) {
-    let answer = 0;
-    let jobsCnt = jobs.length;
-    // 요청시간이 빠른 순서대로 정렬하되 작업의 요청시간이 동일한 경우, 소요시간 기준 오름차순 정렬
-    let jobQueue = jobs.sort((a, b) => {
-        if (a[0] === b[0]) { 
-            return a[1] - b[1] 
-        } else { 
-            return a[0] - b[0]
-        };
-    });
-    let curTime = jobQueue[0][0];
-    let waitQueue = [];
-
-    while (jobQueue.length > 0) {
-        let job = jobQueue[0];
-        // 현재시간이 작업의 요청시간을 지나버렸다면, waitQueue에 작업을 적재
-        if (curTime > job[0]) {
-            waitQueue.push(job);
-            jobQueue.shift();
-        // 현재시간이 작업의 요청시간을 지나지 않았다면,
-        } else {
-            // waitQueue에 작업이 있을 경우 작업 소요시간 기준 오름차순 정렬
-            if (waitQueue.length > 0) {
-                waitQueue.sort((a, b) => a[1] - b[1]);
-                // waitQueue에 있는 작업 '1개'만 처리
-                let waitJob = waitQueue.shift();
-                curTime += waitJob[1];
-                answer += curTime - waitJob[0];
-            // waitQueue에 없으면 바로 작업 처리
-            } else {
-                curTime = job[0] + job[1];
-                answer += job[1];
-                jobQueue.shift();
-            }
-        }
-        
-        if (jobQueue.length === 0 && waitQueue.length > 0) {
-            waitQueue.sort((a, b) => a[1] - b[1]);
-            while (waitQueue.length > 0) {
-                let waitJob = waitQueue.shift();
-                curTime += waitJob[1];
-                answer += curTime - waitJob[0];
-            }
-        }
-    }
+    let waitingTime = 0;
+    let idx = 0;
+    let currentTime = 0;
     
-    return parseInt(answer / jobsCnt);
+    // 요청 시점으로 정렬
+    jobs.sort((a, b) => a[0] - b[0])
+    
+    const priorityQueue = []
+    
+    // 아직 처리되지 않은 작업이 남아 있거나 우선순위 큐에 대기중인 작업이 있을 경우
+    while (idx < jobs.length || priorityQueue.length !== 0) {
+        // 아직 처리되지 않은 작업 중 현재 시간 이전에 들어온 작업들을 우선순위 큐에 삽입
+        if (jobs.length > idx && currentTime >= jobs[idx][0]) {
+            priorityQueue.push(jobs[idx++])
+            // 우선순위 큐에서는 작업의 소요 시간 순으로 정렬
+            priorityQueue.sort((a, b) => a[1] - b[1])
+            continue
+        }
+        // 우선순위 큐에 대기중인 작업이 있을 경우
+        if (priorityQueue.length !== 0) {
+            currentTime += priorityQueue[0][1]
+            waitingTime += currentTime - priorityQueue[0][0]
+            priorityQueue.shift();
+        } else {
+            currentTime = jobs[idx][0]
+        }
+    } return parseInt(waitingTime / jobs.length)
 }
