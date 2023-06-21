e373ad0eb35d7decd39d1a5bd31e5213f55b5e46
summermong <124887974+summermong@users.noreply.github.com>
2023-06-21 11:43:56 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,28 +1,17 @@
 function solution(brown, yellow) {
-  let ans = [];
-
-  for (let i = 1; i <= yellow; i++) {
-    if (i / yellow === 0) {
-      ans.push([1, 1]);
-    } else if (parseInt(yellow / i) * i === yellow) {
-      ans.push([yellow / i, i]);
-    }
-  }
-
   let ans2 = [];
 
-  for (let i = 0; i < ans.length; i++) {
-    if (ans[i][0] >= ans[i][1]) {
-      ans2.push([ans[i][0], ans[i][1]]);
+  for (let i = 1; i * i <= yellow; i++) {
+    if (yellow % i === 0) {
+      if (yellow / i >= i) {
+        ans2.push([yellow / i, i]);
+      }
     }
   }
-
-  let ans3 = [];
 
   for (let i = 0; i < ans2.length; i++) {
-    if (ans2[i][0] * 2 + ans2[i][1] * 2 + 4 === brown) {
-      ans3.push(ans2[i][0], ans2[i][1]);
+    if (ans2[i][0] * 2 + ans2[i][1] * 2 === brown - 4) {
+      return [ans2[i][0] + 2, ans2[i][1] + 2];
     }
   }
-  return ans3.map((v) => v + 2);
 }
