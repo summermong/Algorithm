ff627422fe625381ede913d422a0f9682170cd38
summermong <124887974+summermong@users.noreply.github.com>
2023-06-26 11:02:09 +0900

    [level 2] Title: 소수 찾기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,31 +1,31 @@
 function isPrime(n) {
-	if (n < 2) return false;
-	for (let i = 2; i <= Math.sqrt(n); i++) {
-		if (n % i === 0) return false;
-	}
-	return true;
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false
+    }
+    return true
 }
 
-function dfs(set, arr, fixed) {
-	if (arr.length >= 1) {
-		for (let i = 0; i < arr.length; i++) {
-			let newFixed = fixed + arr[i];
-			let copyArr = [...arr];
-			copyArr.splice(i, 1);
-
-			if (isPrime(parseInt(newFixed))) {
-				set.add(parseInt(newFixed));
-			}
-
-			dfs(set, copyArr, newFixed);
-		}
-	}
+function dfs(set, nums, str) {
+    if (nums.length >= 1) {
+        for (let i = 0; i < nums.length; i++) {
+            let newStr = str + nums[i];
+            let newNums = [...nums];
+            newNums.splice(i, 1);
+            
+            if (isPrime(Number(newStr))) {
+                set.add(Number(newStr));
+            }
+            
+            dfs(set, newNums, newStr)
+        }
+    }
 }
 
 function solution(numbers) {
-	let nums = numbers.split("");
-	let set = new Set();
-
-	dfs(set, nums, '');
-	return set.size;
+    let nums = numbers.split("")
+    let set = new Set();
+    dfs(set, nums, "")
+    
+    return set.size;
 }
