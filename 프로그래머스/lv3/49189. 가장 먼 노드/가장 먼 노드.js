661243da41ecd036bfc16c0604dce0506127642a
summermong <124887974+summermong@users.noreply.github.com>
2023-07-05 01:39:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.41 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,24 +1,25 @@
 function solution(n, edge) {
-  const graph = Array.from(Array(n), () => []);
+  const graph = Array.from(Array(n + 1), () => []);
 
   for (let [from, to] of edge) {
-    graph[from - 1].push(to - 1);
-    graph[to - 1].push(from - 1);
+    graph[from].push(to);
+    graph[to].push(from);
   }
 
-  const visited = [1];
-  const queue = [0];
+  const visited = Array(n + 1).fill(false);
+  const queue = [1];
+  visited[1] = true; // 시작 노드를 방문한 것으로 표시
 
   while (queue.length) {
     const current = queue.shift();
     for (const next of graph[current]) {
       if (!visited[next]) {
+        // 방문하지 않은 노드인 경우에만 처리
         visited[next] = visited[current] + 1;
         queue.push(next);
       }
     }
   }
-
   const max = Math.max(...visited);
   return visited.filter((v) => v === max).length;
 }
