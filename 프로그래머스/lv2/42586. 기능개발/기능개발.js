fd90412610546ec00135876dd93f275a741bd3f1
summermong <124887974+summermong@users.noreply.github.com>
2023-07-05 17:24:05 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,20 +1,18 @@
 function solution(progresses, speeds) {
-  const stack = []; // 스택
-  const answer = []; // 결과 배열
-
-  // 작업의 진도와 개발 속도를 순차적으로 처리
-  for (let i = 0; i < progresses.length; i++) {
-    const remaining = Math.ceil((100 - progresses[i]) / speeds[i]); // 남은 작업 일수 계산
-
-    if (stack.length === 0 || stack[stack.length - 1] < remaining) {
-      // 스택이 비어있거나, 현재 작업의 남은 일수가 스택의 가장 위에 있는 일수보다 큰 경우
-      stack.push(remaining); // 스택에 현재 작업의 남은 일수 추가
-      answer.push(1); // 배포되는 기능 개수 1로 초기화
-    } else {
-      // 현재 작업의 남은 일수가 스택의 가장 위에 있는 일수보다 작거나 같은 경우
-      answer[answer.length - 1]++; // 기능 개수 1 증가
+    const stack = []
+    const answer = []
+    
+    for (let i = 0; i < progresses.length; i++) {
+        const day = Math.ceil((100 - progresses[i]) / speeds[i])
+        
+        if (stack.length === 0 || stack[stack.length-1] < day) {
+            stack.push(day)
+            answer.push(1)
+        } else {
+            answer[answer.length-1]++
+        }
     }
-  }
+    
+    return answer
 
-  return answer;
-}
+} 
