7fdcacfb0334d61c320505dc1185ebb62a6123a8
summermong <124887974+summermong@users.noreply.github.com>
2023-07-05 18:27:46 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 176 ms, Memory: 11176 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -0,0 +1,31 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [ N, M ] = input.shift().split(' ').map(v => +v);
+const bacon = new Array(N+1).fill(0);
+const graph = [...new Array(N+1)].map(() => []);
+
+input.forEach(v => {
+  const [ start, end ] = v.split(' ').map(v => +v);
+  graph[start].push(end);
+  graph[end].push(start);
+});
+
+const BFS = start => {
+  const visited = new Array(N+1).fill(false);
+  const queue = [[start, 0]];
+
+  while (queue.length) {
+    let [ node, count ] = queue.shift();
+    if (!visited[node]) {
+      visited[node] = true;
+      bacon[start] += count++;
+      graph[node].forEach(v => queue.push([v, count]));
+    }
+  }
+};
+
+for (let i=1; i<=N; i++) {
+  BFS(i);
+}
+
+bacon.shift();
+console.log(bacon.indexOf(Math.min(...bacon)) + 1);
