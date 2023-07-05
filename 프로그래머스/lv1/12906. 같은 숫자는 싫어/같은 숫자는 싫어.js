81af29bacd6bb2f8e2dc2d94bb4c2c815c6ae22a
summermong <124887974+summermong@users.noreply.github.com>
2023-07-05 17:28:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.71 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,9 @@
 function solution(arr) {
-  let stack = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i + 1]) {
-      stack.push(arr[i]);
-    } 
-  }
-  return stack;
+    let stack = []
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (stack.length === 0 || stack[stack.length-1] !== arr[i]) {
+            stack.push(arr[i])
+        }
+    } return stack
 }
