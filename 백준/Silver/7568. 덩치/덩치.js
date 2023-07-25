a2912fe1edd15bca2b9f55642aa91f3022efed6d
summermong <124887974+summermong@users.noreply.github.com>
2023-07-25 12:28:23 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.js
+++ 백준/Silver/7568. 덩치/덩치.js
@@ -3,19 +3,22 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let num = Number(input.shift());
-let people = input.map((v) => v.split(" ").map((v) => Number(v)));
+let people = [];
+
+for (let i = 0; i < input.length; i++) {
+  people.push(input[i].split(" ").map(Number));
+}
+
 let ranking = [];
 
 for (let i = 0; i < num; i++) {
-  let rank = 0;
+  let rank = 1;
   for (let j = 0; j < num; j++) {
-    if (i === j) continue;
-
     if (people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
       rank++;
     }
   }
-  ranking.push(rank + 1);
+  ranking.push(rank);
 }
 
 console.log(ranking.join(" "));
