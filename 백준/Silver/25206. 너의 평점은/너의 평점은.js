aa461cd8663139822c6fea4d1b00d641e50b5f3b
summermong <124887974+summermong@users.noreply.github.com>
2023-07-27 12:43:25 +0900

    [Silver V] Title: 너의 평점은, Time: 140 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.js
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
 
-let grades = {
+const grades = {
   "A+": 4.5,
   A0: 4.0,
   "B+": 3.5,
@@ -14,25 +14,18 @@ let grades = {
   F: 0.0,
 };
 
-let subjectArr = [];
-let scoreArr = [];
-let gradeArr = [];
+let totalGradePoints = 0;
+let totalCredits = 0;
 
-let totalGrade = 0;
-let totalNum = 0;
+for (const line of input) {
+  const [subject, score, grade] = line.split(" ");
 
-for (let i = 0; i < input.length; i++) {
-  let [sub, scr, gra] = input[i].split(" ");
-  subjectArr.push(sub);
-  scoreArr.push(scr);
-  gradeArr.push(gra);
-}
-
-for (let i = 0; i < gradeArr.length; i++) {
-  if (gradeArr[i] !== "P") {
-    totalGrade += Number(scoreArr[i] * grades[gradeArr[i]]);
-    totalNum += Number(scoreArr[i]);
+  if (grade !== "P") {
+    const gradeValue = grades[grade];
+    totalGradePoints += Number(score) * gradeValue;
+    totalCredits += Number(score);
   }
 }
 
-console.log((totalGrade / totalNum).toFixed(6));
+const gpa = totalGradePoints / totalCredits;
+console.log(gpa.toFixed(6));
