f3ae9c072d44fe6d3d2701449607059d78a6cfe5
summermong <124887974+summermong@users.noreply.github.com>
2023-07-27 10:58:35 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -2,4 +2,12 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim();
 
-console.log(input.replace(/c=|c-|dz=|d-|lj|nj|s=|z=|./g, "a").length);
+let answer = [];
+const croatianAlphabets = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+
+for (const alphabet of croatianAlphabets) {
+  let regex = new RegExp(alphabet, "g");
+  input = input.replace(regex, " ");
+}
+
+console.log(input.split("").length);
