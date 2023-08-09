bef016491486466577be1acbe1e0d6d6ebf0b406
summermong <124887974+summermong@users.noreply.github.com>
2023-08-09 16:09:58 +0900

    [level 2] Title: 숫자의 표현, Time: 1.42 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,21 @@
+function solution(n) {
+    let count = 0;
+    
+    for (let start = 1; start <= n; start++) {
+        let sum = 0;
+        
+        for (let current = start; current <= n; current++) {
+            sum += current;
+            
+            if (sum === n) {
+                count++;
+                break; 
+            } else if (sum > n) {
+                break; 
+            }
+        }
+    }
+    
+    return count;
+}
+
