066eebce73d6e2162ac0383617c5a103f45c7bb4
summermong <124887974+summermong@users.noreply.github.com>
2023-08-12 23:44:44 +0900

    [unrated] Title: 추억 점수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -4,14 +4,14 @@ function solution(name, yearning, photo) {
     dic[name[i]] = yearning[i];
   }
 
-  let answer = [];
+  let ans = [];
 
   for (let i = 0; i < photo.length; i++) {
     let score = 0;
     for (let j = 0; j < photo[i].length; j++) {
       dic[photo[i][j]] ? (score += dic[photo[i][j]]) : 0;
     }
-    answer.push(score);
+    ans.push(score);
   }
-  return answer;
+  return ans;
 }
