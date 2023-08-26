7846509afb21921ea9e57de4356f4af15f803367
summermong <124887974+summermong@users.noreply.github.com>
2023-08-26 23:42:03 +0900

    [level 2] Title: 하노이의 탑, Time: 4.95 ms, Memory: 48.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,15 +1,15 @@
 function solution(n) {
-    let answer = []
+    let count = []
     
     function hanoi (n, start, end, via) {
         if (n === 1) {
-            answer.push([start, end])
+            count.push([start, end])
             return
         } 
         hanoi (n-1, start, via, end)
-        answer.push([start, end])
+        count.push([start, end])
         hanoi (n-1, via, end, start)
     }
     hanoi (n, 1, 3, 2)
-    return answer
+    return count
 }
