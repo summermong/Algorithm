f43a86b8a527ffa72c7d78661ca5d75bf71bf059
summermong <124887974+summermong@users.noreply.github.com>
2023-08-29 23:32:55 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,17 +1,17 @@
 function solution(brown, yellow) {
-  let ans2 = [];
+  let carpet = [];
 
   for (let i = 1; i * i <= yellow; i++) {
     if (yellow % i === 0) {
       if (yellow / i >= i) {
-        ans2.push([yellow / i, i]);
+        carpet.push([yellow / i, i]);
       }
     }
   }
 
-  for (let i = 0; i < ans2.length; i++) {
-    if (ans2[i][0] * 2 + ans2[i][1] * 2 === brown - 4) {
-      return [ans2[i][0] + 2, ans2[i][1] + 2];
+  for (let i = 0; i < carpet.length; i++) {
+    if (carpet[i][0] * 2 + carpet[i][1] * 2 === brown - 4) {
+      return [carpet[i][0] + 2, carpet[i][1] + 2];
     }
   }
 }
