c69a678965fb41989ce74aab4444b7b3b96a9de1
summermong <124887974+summermong@users.noreply.github.com>
2023-09-10 23:51:18 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,21 +1,26 @@
-function dfs(computers, visited, node) {
-  visited[node] = true;
-  for (let i = 0; i < computers[node].length; i++) {
-    if (computers[node][i] === 1 && !visited[i]) {
-      dfs(computers, visited, i);
-    }
-  }
-}
+let arr;
+let visitArr;
 
 function solution(n, computers) {
-  const visited = Array(n).fill(false);
-  let answer = 0;
+    let ctr = 0;
+    arr = computers;
+    visitArr = new Array(n).fill(false);
 
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      dfs(computers, visited, i);
-      answer++;
+    for(let i in arr) {
+        ctr += dfs(i);
     }
-  }
-  return answer;
+
+    return ctr;
+}
+
+function dfs(i) {
+    if(visitArr[i] == true) return 0;
+    else visitArr[i] = true;
+
+    for(let j in arr[i]) {
+        if(arr[i][j] == 1)
+            dfs(j);
+    }
+
+    return 1;
 }
