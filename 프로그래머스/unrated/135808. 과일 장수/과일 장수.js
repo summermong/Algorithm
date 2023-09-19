8ee71dc656c4aced3eba911ce907c7f5745f7140
summermong <124887974+summermong@users.noreply.github.com>
2023-09-19 13:45:59 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,7 +1,7 @@
 function solution(k, m, score) {
     const sortScore = score.sort((a, b) => b - a);
     let answer = 0;
-    for (let i = 0; i < sortScore.length; i += m) {
+    for (let i = 0; i < sortScore.length; i+=m) {
         const box = sortScore.slice(i, i + m);
         if (box.length === m) {
             answer += box[m - 1] * m;
