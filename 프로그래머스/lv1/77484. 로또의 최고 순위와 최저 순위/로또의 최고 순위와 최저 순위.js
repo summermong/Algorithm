8b3b3e0c6770f2a1c4fd3ff8fb180beb5f07abe9
summermong <124887974+summermong@users.noreply.github.com>
2023-09-26 14:48:07 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -2,9 +2,9 @@ function solution(lottos, win_nums) {
     
     let rank = { 0:6, 1:6, 2:5, 3:4, 4:3, 5:2, 6:1}
     
-    let minCount = lottos.filter(v => win_nums.includes(v)).length;
-    let zeroCount = lottos.filter(v => !v).length;
-    const maxCount = minCount + zeroCount;
+    let minCount = lottos.filter(v => win_nums.includes(v)).length; // 일치한 개수(최저)
+    let zeroCount = lottos.filter(v => !v).length; // 0의 개수
+    const maxCount = minCount + zeroCount; // 일치 + 0 (최고)
 
     return [rank[maxCount], rank[minCount]]
 }
