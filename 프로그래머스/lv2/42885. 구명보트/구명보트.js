b93b7530666db0181c0c3432dce68ea07410ff34
summermong <124887974+summermong@users.noreply.github.com>
2023-09-30 23:42:34 +0900

    [level 2] Title: 구명보트, Time: 28.60 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,19 +1,7 @@
 function solution(people, limit) {
-  let ans = 0;
-  people.sort((a, b) => a - b);
-
-  let left = 0;
-  let right = people.length - 1;
-
-  while (left <= right) {
-    if (people[left] + people[right] <= limit) {
-      left++;
-      right--;
-    } else {
-      right--;
-    }
-    ans++;
-  }
-
-  return ans;
+    people.sort(function(a, b){return a-b});
+    for(var i=0, j=people.length-1; i < j; j--) {
+        if( people[i] + people[j] <= limit ) i++;
+    }    
+    return people.length-i;
 }
