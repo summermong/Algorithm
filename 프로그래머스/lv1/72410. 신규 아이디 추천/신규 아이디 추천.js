057918e54ba52365690f3ad6a754180089fb9f4a
summermong <124887974+summermong@users.noreply.github.com>
2023-10-03 14:28:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,11 +1,12 @@
 function solution(new_id) {
     let answer = new_id
-    .toLowerCase()
-    .replace(/[^\w-_.]/g, "")
-    .replace(/\.+/g, ".")
-    .replace(/^\.|\.$/g, "")
-    .padEnd(1, "a")
-    .slice(0, 15)
-    .replace(/\.$/g, "")
-    return answer.length <= 2 ? answer.padEnd(3, answer[answer.length-1]) : answer
-}
+    .toLowerCase() // 1단계 소문자로 치환
+    .replace(/[^\w-_.]/g, "") // 2단계 소문자, 숫자, -, _, . 만 남기기
+    .replace(/\.+/g, ".") // 3단계 마침표가 2번 이상 연속이면 1개로 치환
+    .replace(/^\.|\.$/g, "") // 4단계 마침표가 처음이나 끝에 있다면 제거
+    .padEnd(1, "a") // 5단계 빈 문자열이라면 a를 대입
+    .slice(0, 15) // 6단계 길이가 0 ~ 15개의 문자만 남기기
+    .replace(/\.$/g, "") // 6단계 후 4단계의 뒷부분과 동일
+    return answer.length <= 2 ? 
+        answer.padEnd(3, answer[answer.length-1]) : answer // 7단계 길이에 따라 추가 및 반환
+}  
