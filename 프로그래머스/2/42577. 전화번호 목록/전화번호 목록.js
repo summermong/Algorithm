b33dec337c078c7d043479d918fddcb8ffeee242
summermong <124887974+summermong@users.noreply.github.com>
2023-11-02 14:11:08 +0900

    [level 2] Title: 전화번호 목록, Time: 117.35 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -6,7 +6,7 @@ function solution(phone_book) {
 
   for (let i = 0; i < arrNum.length - 1; i++) {
     let curNumLeng = arrNum[i].length;
-    let nextNum = arrNum[i + 1].substr(0, curNumLeng);
+    let nextNum = arrNum[i + 1].slice(0, curNumLeng);
 
     if (arrNum[i] === nextNum) {
       return false;
