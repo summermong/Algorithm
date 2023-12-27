91b26d2c8b28f8e353102422f8d1d44158713348
summermong <124887974+summermong@users.noreply.github.com>
2023-12-27 17:14:53 +0900

    [level 2] Title: 튜플, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,27 +1,40 @@
 function solution(s) {
-  let pair = [];
-  let num = s
-    .replaceAll('{', '')
-    .replaceAll('}', '')
-    .split(',')
-    .map((v) => Number(v));
-  let map = new Map();
+  const num = s.replace(/{|}/g, '').split(',').map(Number);
+  const map = new Map();
+  num.forEach((v) => map.set(v, (map.get(v) || 0) + 1));
 
-  for (let i = 0; i < num.length; i++) {
-    map.set(num[i], (map.get(num[i]) || 0) + 1);
-  }
+  const sortedPairs = Array.from(map).sort((a, b) => b[1] - a[1]);
 
-  let pairs = map.forEach((i, v) => {
-    pair.push([v, i]);
-  });
-
-  pair.sort((a, b) => b[1] - a[1]);
-
-  let answer = [];
-
-  for (let i = 0; i < pair.length; i++) {
-    answer.push(pair[i][0]);
-  }
+  const answer = sortedPairs.map(([key]) => key);
 
   return answer;
 }
+
+
+// function solution(s) {
+//   let pair = [];
+//   let num = s
+//     .replaceAll('{', '')
+//     .replaceAll('}', '')
+//     .split(',')
+//     .map((v) => Number(v));
+//   let map = new Map();
+
+//   for (let i = 0; i < num.length; i++) {
+//     map.set(num[i], (map.get(num[i]) || 0) + 1);
+//   }
+
+//   let pairs = map.forEach((i, v) => {
+//     pair.push([v, i]);
+//   });
+
+//   pair.sort((a, b) => b[1] - a[1]);
+
+//   let answer = [];
+
+//   for (let i = 0; i < pair.length; i++) {
+//     answer.push(pair[i][0]);
+//   }
+
+//   return answer;
+// }
